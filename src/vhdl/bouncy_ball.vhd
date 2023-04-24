db71d4f8965befe11cbd3f5ac443b10d3f0700a7
Ian Kuik <100252099+ikui753@users.noreply.github.com>
2023-04-25 09:10:54 +1200

    changed sens list

--- src/vhdl/bouncy_ball.vhd
+++ src/vhdl/bouncy_ball.vhd
@@ -16,7 +16,7 @@ architecture behavior of bouncy_ball is
 SIGNAL ball_on					: std_logic;
 SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
 SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0);
-SiGNAL ball_x_pos				: std_logic_vector(10 DOWNTO 0);
+SIGNAL ball_x_pos				: std_logic_vector(10 DOWNTO 0);
 SIGNAL ball_y_motion			: std_logic_vector(9 DOWNTO 0);
 
 BEGIN           
@@ -37,24 +37,27 @@ Green <= (not pb2) and (not ball_on);
 Blue <=  not ball_on;
 
 
-Move_Ball: process (left_button)  -- add left_button to sens list
+Move_Ball: process (vert_sync)  -- add left_button to sens list
 begin
 	-- Move ball on mouse click rising edge and vert_sync rising edge
-	if (Rising_Edge(left_button) and Rising_Edge(vert_sync)) then			
+	if (Rising_Edge(vert_sync)) then			
 		-- Bounce off top or bottom of the screen DON'T NEED THIS BUT WILL KEEP
 		--if ( ('0' & ball_y_pos >= CONV_STD_LOGIC_VECTOR(479,10) - size) ) then
 		--	ball_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
 		--elsif (ball_y_pos <= size) then 
 		--	ball_y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
 		--end if;
-		
-		-- Compute next ball Y position
-		ball_y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
-		ball_y_pos <= ball_y_pos + ball_y_motion;
-	else 
-		-- GRAVITY COMES HERE this is a place holder
-		ball_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
-		ball_y_pos <= ball_y_pos - ball_y_motion;
+		if(left_button) then
+			-- Compute next ball Y position
+			ball_y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
+			ball_y_pos <= ball_y_pos + ball_y_motion;
+		else 
+			-- GRAVITY COMES HERE this is a place holder
+			ball_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
+			ball_y_pos <= ball_y_pos - ball_y_motion;
+		end if;
+	end if;
+
 end process Move_Ball;
 
 END behavior;
