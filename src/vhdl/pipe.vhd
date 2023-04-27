e07b7ac22793e4c7c5903d5970f4ae8203eaa629
bcho892 <AKLbc70@gmail.com>
2023-04-28 10:37:43 +1200

    stop motion on reaching edge

--- src/vhdl/pipe.vhd
+++ src/vhdl/pipe.vhd
@@ -10,7 +10,7 @@ entity pipe is
 		game_state: IN STD_LOGIC_VECTOR(2 downto 0);
 		game_level : IN STD_LOGIC_VECTOR(2 downto 0);
 		random_index : IN STD_LOGIC_VECTOR(3 downto 0);
-		init_next, red, green, blue : OUT STD_LOGIC 
+		init_next, pipe_on, red, green, blue: OUT STD_LOGIC 
 	);
 end entity;
 
@@ -30,31 +30,31 @@ CONSTANT screen_max_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(63
 -- SIGNALS
 SIGNAL pipe_x_pos : STD_LOGIC_VECTOR(9 downto 0) := screen_max_x;
 SIGNAL pipe_x_motion : STD_LOGIC_VECTOR(9 downto 0);
-SIGNAL pipe_height : STD_LOGIC_VECTOR(9 downto 0);
-SIGNAL pipe_on : STD_LOGIC; 
+SIGNAL pipe_height : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(20,10);
+SIGNAL temp_pipe_on : STD_LOGIC; 
 SIGNAL enable : STD_LOGIC;
 
 begin
-pipe_height <= CONV_STD_LOGIC_VECTOR(20, 10);
-pipe_x_motion <= CONV_STD_LOGIC_VECTOR(5, 10);
-pipe_on <= '1' when ( 
-(pipe_x_pos <= pixel_column and pixel_column <= pipe_x_pos + pipe_width) and
-(pipe_height + pipe_gap <= pixel_row or (pipe_x_pos <= pixel_column + pipe_width and pixel_column <= pipe_x_pos + pipe_width and pixel_row <= pipe_height))
+temp_pipe_on <= '1' when ( 
+(pipe_x_pos >= pixel_column and pixel_column >= pipe_x_pos - pipe_width) and
+(pipe_height + pipe_gap <= pixel_row or (pipe_x_pos >= pixel_column and pixel_column >= pipe_x_pos - pipe_width and pixel_row <= pipe_height))
 	   ) else	'0';
-green <= pipe_on;
-
+green <= temp_pipe_on;
+pipe_on <= temp_pipe_on;
 move_pipe : process(vert_sync) 	
 begin
 	if Rising_Edge(vert_sync) then
 		if init = '1' then 
 			enable <= '1';
 		end if;
-		-- when to start moving next pipe
-		if enable = '1' then		
-			pipe_x_pos <= pipe_x_pos - pipe_x_motion;
+
+		if '0' & pipe_x_pos > '0' & CONV_STD_LOGIC_VECTOR(639,10) then
+			pipe_x_motion <= CONV_STD_LOGIC_VECTOR(0,10);
 		else 
-			pipe_x_pos <= pipe_x_pos;
+			pipe_x_motion <= CONV_STD_LOGIC_VECTOR(1,10);
 		end if;
+
+		pipe_x_pos <= pipe_x_pos - pipe_x_motion;
 	end if;
 end process move_pipe;
 end behaviour;
