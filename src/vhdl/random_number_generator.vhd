471f3bda25643669a6936670db05360008c3b705
Ian Kuik <100252099+ikui753@users.noreply.github.com>
2023-04-27 12:04:29 +1200

    update random number generator

--- src/vhdl/random_number_generator.vhd
+++ src/vhdl/random_number_generator.vhd
@@ -1,24 +1,21 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
 entity random_number_generator is
-    port(clk, reset: in std_logic;
-    output: out std_logic_vector(2 downto 0)
-    );
-end entity random_number_generator;
+    port (clk : in std_logic;
+          reset : in std_logic;
+          random_out : out std_logic_vector(3 downto 0));
+end random_number_generator;
 
-architecture behaviour of random_number_generator is
-    signal lsfr_reg : std_logic_vector(2 downto 0) := "100"; -- initialise seed
+architecture Behavioral of random_number_generator is
+    signal lfsr_reg : std_logic_vector(3 downto 0) := "0001"; -- Initial seed value
 begin
     process(clk, reset)
     begin
-        if reset = '1' then
-            lsfr_reg <= "100"; -- reset to seed
-        elsif rising_edge(clk) then
-            lfsr_reg <= lfsr_reg(1 downto 0) & (lfsr_reg(2) XOR lfsr_reg(1) XOR lfsr_reg(0));
+        if (reset = '1') then
+            lfsr_reg <= "0001"; -- Reset the LFSR to the initial seed value
+        elsif (rising_edge(clk)) then
+            -- and operation acts as shift to produce next state
+            lfsr_reg <= lfsr_reg(2 downto 0) & (lfsr_reg(3) XOR lfsr_reg(2)); -- LFSR update equation
         end if;
     end process;
 
-    output <= lsfr_reg;
-end behaviour;
+    random_out <= lfsr_reg; -- Output the current state of the LFSR as the pseudo-random 3 bit number
+end Behavioral;
