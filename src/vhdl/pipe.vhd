34aab06145b84f1bb6e528eab3619f6e32128741
bcho892 <AKLbc70@gmail.com>
2023-04-28 16:04:24 +1200

    initialize pipes properly

--- src/vhdl/pipe.vhd
+++ src/vhdl/pipe.vhd
@@ -26,22 +26,28 @@ CONSTANT preset_scroll_speeds : speed_vector := (5, 10, 12, 15, 18);
 CONSTANT preset_pipe_heights : height_vector:= (58, 33, 46, 233, 243, 108, 304, 196, 286, 96, 15, 32, 216, 292, 323, 269, 144);
 CONSTANT pipe_gap : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(120, 10); 
 CONSTANT pipe_width : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(40,10); 
-CONSTANT pipe_spacing : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(120, 10);
+CONSTANT pipe_spacing : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(140, 10);
 CONSTANT screen_max_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(639, 10);
 
 -- SIGNALS
-SIGNAL pipe_x_pos : STD_LOGIC_VECTOR(9 downto 0) := screen_max_x;
+SIGNAL pipe_x_pos : STD_LOGIC_VECTOR(9 downto 0) := screen_max_x + pipe_width;
 SIGNAL pipe_x_motion : STD_LOGIC_VECTOR(9 downto 0);
-SIGNAL pipe_height : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(preset_pipe_heights(1), 10);
+SIGNAL pipe_height : STD_LOGIC_VECTOR(9 downto 0);
 SIGNAL temp_pipe_on : STD_LOGIC; 
 SIGNAL enable : STD_LOGIC;
 SIGNAL current_index : Integer;
 
 begin
 temp_pipe_on <= '1' when ( 
-(pipe_x_pos >= pixel_column and pixel_column >= pipe_x_pos - pipe_width) and
-(pipe_height + pipe_gap <= pixel_row or (pipe_x_pos >= pixel_column and pixel_column >= pipe_x_pos - pipe_width and pixel_row <= pipe_height))
-	   ) else	'0';
+(pipe_x_pos >= pixel_column 
+and pixel_column >= pipe_x_pos - pipe_width 
+and pipe_height + pipe_gap <= pixel_row) 
+or 
+(pipe_x_pos >= pixel_column 
+and pixel_column >= pipe_x_pos - pipe_width 
+and pixel_row <= pipe_height) 
+) else	'0';
+
 green <= temp_pipe_on;
 pipe_on <= temp_pipe_on;
 current_index <= numeric_std.to_integer(numeric_std.unsigned(random_index));
@@ -68,6 +74,8 @@ begin
 		if enable = '1' then
 			pipe_x_motion <= CONV_STD_LOGIC_VECTOR(2,10);
 			pipe_x_pos <= pipe_x_pos - pipe_x_motion;
+		else 
+			pipe_height <= CONV_STD_LOGIC_VECTOR(preset_pipe_heights(current_index), 10);
 		end if;
 
 	end if;
