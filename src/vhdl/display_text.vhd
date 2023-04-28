88759920f4adb5fa5a21da0af768fea594f1ee09
bcho892 <AKLbc70@gmail.com>
2023-04-29 10:39:28 +1200

    got text displaying two digits

--- src/vhdl/display_text.vhd
+++ src/vhdl/display_text.vhd
@@ -6,7 +6,7 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 entity display_text is 
 	port(
 			clk : IN STD_LOGIC;
-			score : IN STD_LOGIC_VECTOR(7 downto 0);
+			score : IN STD_LOGIC_VECTOR(5 downto 0);
 			pixel_row, pixel_column : IN STD_LOGIC_VECTOR(9 downto 0);
 			text_on, red, green, blue : OUT STD_LOGIC 
 		);
@@ -14,14 +14,16 @@ end display_text;
 
 architecture behaviour of display_text is
 --CONSTANTS
-CONSTANT score_start_y : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(100, 10);
-CONSTANT score_start_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(200, 10);
-CONSTANT score_end_y : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(150, 10);
-CONSTANT score_end_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(250, 10);
+CONSTANT score_start_y : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(70, 10);
+CONSTANT score_end_y : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(90, 10);
+CONSTANT score_0_start_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(300, 10);
+CONSTANT score_0_end_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(332, 10);
+CONSTANT score_1_start_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(333, 10);
+CONSTANT score_1_end_x : STD_LOGIC_VECTOR(9 downto 0) := CONV_STD_LOGIC_VECTOR(365, 10);
 
 --SIGNALS
 SIGNAL temp_text_on : STD_LOGIC;
-SIGNAL text_corresponding_address : STD_LOGIC_VECTOR(5 downto 0) := CONV_STD_LOGIC_VECTOR(20, 6);
+SIGNAL score_text_corresponding_address : STD_LOGIC_VECTOR(5 downto 0) := CONV_STD_LOGIC_VECTOR(20, 6);
 SIGNAL current_row, current_col : STD_LOGIC_VECTOR(2 downto 0) := CONV_STD_LOGIC_VECTOR(6,3);
 
 component char_rom 
@@ -36,18 +38,20 @@ end component;
 begin
 char_rom_component : char_rom
 port map(
-			character_address => text_corresponding_address,
+			character_address => score_text_corresponding_address,
 			font_row => current_row,
 			font_col => current_col,
 			clock => clk,
 			rom_mux_output => temp_text_on 
 	 	);
+
+
 current_row <= pixel_row(4 downto 2) - score_start_y(4 downto 2);
-current_col <= pixel_column(4 downto 2) - score_start_x(4 downto 2);
+current_col <= pixel_column(4 downto 2) - score_0_start_x(4 downto 2);
 red <= temp_text_on;  
 text_on <= temp_text_on when
 (pixel_row <= score_end_y and score_start_y <= pixel_row) 
-			  and (pixel_column <= score_end_x and score_start_x <= pixel_column) else '0';
+			  and (pixel_column <= score_1_end_x and score_0_start_x <= pixel_column) else '0';
 
 end architecture behaviour;
 
