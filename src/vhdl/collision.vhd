a9dcb76d805e85859862ec0561fa65eb103f15b8
bcho892 <AKLbc70@gmail.com>
2023-05-01 13:26:41 +1200

    add symbol file for collision

--- src/vhdl/collision.vhd
+++ src/vhdl/collision.vhd
@@ -1,22 +1,25 @@
-library ieee;
-use NUMERIC_STD;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_SIGNED.all;
+USE IEEE.NUMERIC_STD;
 
 entity collision is 
-    PORT (bird_on, pipe_on, vert_sync: in std_logic;
+	PORT (bird_on, pipe_on, vert_sync: in std_logic;
             collision_detected: out std_logic      
     );
 end entity collision;
 
 architecture behaviour of collision is
-    
+begin  
     process(vert_sync)
     begin
         if(Rising_Edge(vert_sync)) then
             -- check collision
-            if(bird_on and pipe_on) then
-                collision_detected <= '1';
-            else
+            if(bird_on = '1' and pipe_on = '1') then
                 collision_detected <= '0';
+            else
+                collision_detected <= '1';
             end if;
         end if;
     end process;
